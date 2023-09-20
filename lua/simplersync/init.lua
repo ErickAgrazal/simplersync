22400be49dfbf59506d5be9e5abf948b5a5d6a74
Erick Vicente Agrazal Lopez <erick@agrazal.com>
2023-09-20 08:42:06 -0500

    Added hook for onsave event.

--- lua/simplersync/init.lua
+++ lua/simplersync/init.lua
@@ -1,10 +1,25 @@
 local sync = require("simplersync.sync")
 
-vim.api.nvim_create_user_command("SimpleRsync", function(opts)
+local function execute_sync_up()
     local current_dir = vim.fn.expand("%:p:h")
     local local_file = vim.fn.expand("%:.")
     sync.sync_up(local_file, current_dir)
-end, {
+end
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    callback = function()
+        -- only initialize once per buffer
+        if vim.b.rsync_init == nil then
+            vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+                callback = execute_sync_up,
+                buffer = vim.api.nvim_get_current_buf(),
+            })
+            vim.b.rsync_init = 1
+        end
+    end,
+})
+
+vim.api.nvim_create_user_command("SimpleRsync", execute_sync_up, {
     nargs = "?",
     complete = function(ArgLead, CmdLine, CursoPos)
         return { "disable", "toggle", "enable" }
